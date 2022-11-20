df0b84ef880c68f1cbf1741efa4f46052a07080c
lrrmonte <lincoln.rubim@gmail.com>
2022-11-20 02:38:23 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,6 @@
 
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 
@@ -13,9 +14,9 @@ type Props = {
 function handleClick(id :number){
   axios(`${BASE_URL}/sales/${id}/notification`)
   .then(response => { 
-    console.log("Sucesso")
+    toast.info("SMS enviado com sucesso");
     
-  })
+  });
 }
 
 function NotificationButton({saleId}:Props) {
